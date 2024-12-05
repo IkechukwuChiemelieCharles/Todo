26038e0dddb98517974fa0ad1940ddb308635077
Ikechukwu Chiemelie Charles <chiemeliecharleschristopher@gmail.com>
2024-12-05 02:01:47 +0100

    Added the item count feature

--- data.js
+++ data.js
@@ -13,6 +13,11 @@ const lightToggle = document.querySelector("#lightToggle");
 
 const bodyDark = document.getElementsByTagName("body");
 
+const itemCount = document.querySelector(".itemCount");
+const deleteCompleted = document.querySelector(".deleteCompleted");
+
+const panel2 = document.querySelector(".panel2");
+
 // darkMode
 
 const inputContainer = document.querySelector(".inputContainer");
@@ -57,6 +62,14 @@ function displayTodo(e) {
 
   //appended the list item into the list container
   listContainer.prepend(listItem);
+
+  // itemCount.textContent = "Hey";
+  // console.log(listContainer.length);
+  const lengths = (itemCount.textContent = `${
+    listContainer.children.length - 1
+  }   ${listContainer.children.length === 2 ? "item" : "items"}  left`);
+
+  // console.log(.children.length - 1);
 }
 
 listContainer.addEventListener("click", deleteCheck);
@@ -69,10 +82,11 @@ function deleteCheck(e) {
     todo.classList.add("hide");
     todo.addEventListener("animationend", function () {
       todo.remove();
-      console.log("trans");
     });
 
-    // console.log("remove");
+    itemCount.textContent = `${listContainer.children.length - 2} ${
+      listContainer.children.length === 3 ? "item" : "items"
+    }  left`;
   }
 
   if (item.classList[0] === "checked") {
@@ -102,6 +116,7 @@ function deleteCheck(e) {
 all.addEventListener("click", filter);
 completed.addEventListener("click", filter);
 active.addEventListener("click", filter);
+deleteCompleted.addEventListener("click", filter);
 
 function filter(e) {
   const todos = listContainer.childNodes;
@@ -118,12 +133,10 @@ function filter(e) {
         completed.classList.remove("color");
 
         todo.style.display = "flex";
-        console.log(e.target);
 
         break;
 
       case "Active":
-        console.log("click");
         all.classList.remove("color");
         completed.classList.remove("color");
         e.target.classList.add("color");
@@ -151,6 +164,30 @@ function filter(e) {
         } else {
           todo.style.display = "none";
         }
+
+        break;
+      case "Clear Completed":
+        if (todo.classList.contains("inactive")) {
+          console.log(todo);
+
+          todo.remove();
+          itemCount.textContent = `${listContainer.children.length - 1} ${
+            listContainer.children.length === 2 ? "item" : "items"
+          }  left`;
+        }
+      //     console.log("kinos");
+      //   } else {
+      //     // todo.style.display = "flex";
+      //   }
+
+      // for (const item of todos) {
+      //   if (item.classList.contains("inactive")) {
+      //     item.remove();
+      //     console.log("kinos");
+      //   } else {
+      //     // todo.style.display = "flex";
+      //   }
+      // }
     }
   });
 }
@@ -159,7 +196,6 @@ darkToggle.addEventListener("click", darkMode);
 lightToggle.addEventListener("click", lightMode);
 
 function darkMode() {
-  console.log("het");
   body.classList.add("theme2");
   darkToggle.classList.add("hidden");
   lightToggle.classList.remove("hidden");
@@ -169,3 +205,35 @@ function lightMode() {
   lightToggle.classList.add("hidden");
   body.classList.remove("theme2");
 }
+
+// deleteCompleted.addEventListener("click", deleteAllCompleted);
+
+// function deleteAllCompleted(e) {
+//   const comp = listContainer.childNodes;
+
+//   console.log(comp);
+
+// comp.forEach(function (comps) {
+//   if (e.target.classList) {
+//   }
+// });
+
+// if (compChild.classList.contains("inactive")) {
+// }
+
+// function filter(e) {
+// const todos = listContainer.childNodes;
+
+// todos.forEach(function (todo) {
+//   switch (e.target.innerText) {
+//     case "All":
+//       e.target.style.opacity = 1;
+//       active.style.opacity = 0.4;
+//       completed.style.opacity = 0.4;
+
+//       e.target.classList.add("color");
+//       active.classList.remove("color");
+//       completed.classList.remove("color");
+
+//       todo.style.display = "flex";
+//       console.log(e.target);
